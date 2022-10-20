d4106af8cd0b95d79237cceec51e3e64a6241b9b
devarohman <devarohman@gmail.com>
2022-10-20 15:57:40 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -9,5 +9,9 @@
 <body>
     <h1>Hello World!</h1>
     <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illum ipsum maiores culpa. Aut nesciunt ad non at, rerum natus nemo eius labore consectetur, aperiam sunt possimus provident odio delectus harum!</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
